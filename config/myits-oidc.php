3b87b241f7b1a8a78de904b92962537e9c572d73
Ifan Iqbal <ifaniqbal.com@gmail.com>
2020-11-30 15:44:15 +0700

    Fix typo on config key

--- config/myits-oidc.php
+++ config/myits-oidc.php
@@ -8,5 +8,5 @@ return [
     'redirect_path'     => env('MYITS_OIDC_REDIRECT_PATH', 'myits-sso'),
     'api_auth_endpoint' => env('MYITS_OIDC_API_AUTH_ENDPOINT', 'https://my.its.ac.id'),
     'api_client_id'     => env('MYITS_OIDC_API_CLIENT_ID', env('MYITS_OIDC_ID')),
-    'api_client_secret' => env('MYITS_OIDC_API_CLIENT_SECRET', env('OIDC_SECRET')),
+    'api_client_secret' => env('MYITS_OIDC_API_CLIENT_SECRET', env('MYITS_OIDC_SECRET')),
 ];
